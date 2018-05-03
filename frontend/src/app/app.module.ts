86bc1dcacae244db769e691906717abb999c870e
RayedB <rayed.benbrahim@gmail.com>
2018-05-03 10:30:27 +0100

    init frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular'
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    
+  }
+}
